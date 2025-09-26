fd80d23db5d789454d461518c39d65c6bfd46e79
mikl0w <mikkelklitgaard@live.dk>
2025-09-26 12:25:12 +0200

    Update CustomerType and add B2nDiscountStrategy
    Modified the `CustomerType` enum to add `B2N`.
    Introduced `B2nDiscountStrategy` class implementing `IDiscountStrategy`,
    which provides a 10% discount for orders from `B2N` customers.

---
+++ Maleren.Domain/Discounts/DiscountStrategies/B2nDiscountStrategy.cs
@@ -0,0 +1,16 @@
+﻿using Maleren.Domain.Customers;
+using Maleren.Domain.Orders;
+
+namespace Maleren.Domain.Discounts.DiscountStrategies
+{
+    public class B2nDiscountStrategy : IDiscountStrategy
+    {
+        public Discount CalculateDiscount(Order order)
+        {
+            if (order.Customer.CustomerType != CustomerType.B2N)
+                return new Discount(GetType().Name, 0, false);
+
+            return new Discount(GetType().Name, order.CalculateOrderTotal() * 0.1m, true);
+        }
+    }
+}
