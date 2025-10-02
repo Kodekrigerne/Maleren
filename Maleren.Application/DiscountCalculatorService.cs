e7c7421abb6b5f3013c848c2e37b63e2900490ff
MDahlmann <dahlmann.mikkel@gmail.com>
2025-10-02 12:49:44 +0200

    Add DiscountCalculatorService and update discount logic
    Introduced `DiscountCalculatorService` to calculate the best
    discount for orders using parallel processing and discount
    strategies. Updated `IDiscountCalculatorService` to return
    nullable discounts. Added a project reference to
    `Maleren.Domain` in `Maleren.Application.csproj`.
    
    Included a `TODO` comment in the `Order` class for handling
    discounts after calculation. Added necessary `using`
    directives in `DiscountCalculatorService` for required
    namespaces.

---
+++ Maleren.Application/DiscountCalculatorService.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Concurrent;
+using Maleren.Domain.Discounts;
+using Maleren.Domain.Orders;
+
+namespace Maleren.Application;
+
+public class DiscountCalculatorService : IDiscountCalculatorService
+{
+    public List<IDiscountStrategy> DiscountStrategies { get; protected set; } = [];
+
+    Discount? IDiscountCalculatorService.GetBestDiscount(Order order)
+    {
+        ConcurrentBag<Discount> discounts = [];
+
+        Parallel.ForEach(DiscountStrategies, discountStrategy =>
+        {
+            discounts.Add(discountStrategy.CalculateDiscount(order));
+        });
+
+        return discounts.Where(x => x.DiscountActive).MaxBy(x => x.DiscountAmount);
+    }
+}
