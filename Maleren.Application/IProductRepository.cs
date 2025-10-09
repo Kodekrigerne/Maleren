cd4b469e7b9918058307bc795543753ba42a204c
mikl0w <mikkelklitgaard@live.dk>
2025-10-09 10:44:58 +0200

    Updated to async methods

--- Maleren.Application/IProductRepository.cs
+++ Maleren.Application/IProductRepository.cs
@@ -4,9 +4,9 @@ namespace Maleren.Application
 {
     public interface IProductRepository
     {
-        void AddProduct(Product product);
-        void DeleteProduct(Product product);
-        Product GetProductByGuid(Guid id);
-        void SaveProduct(Product product);
+        Task AddProductAsync(Product product);
+        Task DeleteProductAsync(Product product);
+        Task<Product> GetProductByGuidAsync(Guid id);
+        Task SaveProductAsync(Product product);
     }
 }
