5a2533f1c3f513c68664a98865e9699ba1fc69a9
Dennis Johnsen <dennisjohnsenucl@gmail.com>
2025-10-09 11:06:13 +0200

    Updated OrderCustomer and Orders handling of OrderCustomer.

--- Maleren.Domain/Orders/OrderCustomer.cs
+++ Maleren.Domain/Orders/OrderCustomer.cs
@@ -1,6 +1,22 @@
-﻿using Maleren.Domain.Customers;
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using Maleren.Domain.Customers;
 
 namespace Maleren.Domain.Orders
 {
-    public record OrderCustomer(Guid Id, CustomerType CustomerType);
+    [ComplexType]
+    public record OrderCustomer
+    {
+        public Guid Id { get; }
+        public CustomerType CustomerType { get; }
+
+        protected OrderCustomer() { }
+
+        private OrderCustomer(Customer customer)
+        {
+            Id = customer.Id;
+            CustomerType = customer.CustomerType;
+        }
+
+        public static OrderCustomer FromCustomer(Customer customer) => new(customer);
+    }
 }
